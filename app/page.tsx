189f86efea6a91b54bbb745b891529688163dbfd
arttop1000 <arthurbarbosams@yahoo.com>
2026-06-27 17:05:45 -0300

    fix: correct homepage file

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,3 @@
-import { getAllPosts } from "@/lib/posts";
-import Link from "next/link";
-
 export default function Home() {
-  const posts = getAllPosts();
-
-  return (
-    <main>
-      <h1>Blog</h1>
-
-      {posts.map((post: any) => (
-        <div key={post.slug}>
-          <Link href={`/blog/${post.slug}`}>
-            <h2>{post.title}</h2>
-          </Link>
-          <p>{post.date}</p>
-        </div>
-      ))}
-    </main>
-  );
+  return <h1>HOME OK</h1>;
 }
